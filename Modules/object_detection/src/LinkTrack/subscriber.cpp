d83c61b977d868e6c0dbfffec4cbf885988a2b5b
ydm-P53 <1779876755@qq.com>
2020-11-26 17:21:24 +0800

    add Nlink Module driver

---
+++ Modules/object_detection/src/LinkTrack/subscriber.cpp
@@ -0,0 +1,18 @@
+#include <nlink_parser/LinktrackNodeframe2.h>
+#include <ros/ros.h>
+
+void tagframe0Callback(const nlink_parser::LinktrackNodeframe2 &msg) {
+  ROS_INFO("msg LinktrackNodeframe2 received,systemTime: %d", msg.system_time);
+}
+
+int main(int argc, char **argv) {
+  ros::init(argc, argv, "linktrack_example");
+  ros::NodeHandle nh;
+
+  ros::Subscriber sub =
+      nh.subscribe("nlink_linktrack_nodeframe2", 1000, tagframe0Callback);
+
+  ros::spin();
+
+  return 0;
+}
