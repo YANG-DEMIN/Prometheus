7bc3af9c36dde125996c36c9413939b1ef146a31
ydm-P53 <1779876755@qq.com>
2021-01-14 21:35:47 +0800

    add nlink_parser to compile file

--- compile_experiment.sh
+++ compile_experiment.sh
@@ -1,6 +1,7 @@
 catkin_make --source Modules/common/msgs --build build/msgs
 catkin_make --source Modules/control --build build/control
 catkin_make --source Modules/mission --build build/mission
+catkin_make --source Modules/nlink_parser --build build/nlink_parser
 
 catkin_make --source Modules/test_nlink --build build/test_nlink
 catkin_make --source Experiment --build build/prometheus_experiment
